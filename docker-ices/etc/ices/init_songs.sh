422d1074a72ce6fae05693653b7e351d645a9b59
Greg Holthaus <linuxguy83@gmail.com>
2022-04-22 18:33:19 -0500

    Fixed copy/paste issue

--- docker-ices/etc/ices/init_songs.sh
+++ docker-ices/etc/ices/init_songs.sh
@@ -5,7 +5,7 @@
 
 printf "Checking song filenames...\n"
 
-for file in /data/station_0/*.ogg;                                                                                                                                                                       ✔  16:10:50  
+for file in /data/station_0/*.ogg; 
 do
 echo Converting "$file" to "${file// /_}"
 mv "$file" "${file// /_}"
