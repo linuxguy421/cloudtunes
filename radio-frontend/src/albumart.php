a7112d672063be4ed7638a9a7d131aaa49cbe1d4
Greg Holthaus <linuxguy83@gmail.com>
2023-02-01 18:06:58 -0600

    Fix NGINX delayed start issue

--- radio-frontend/src/albumart.php
+++ radio-frontend/src/albumart.php
@@ -1,6 +1,6 @@
 <?php
 $m = new Memcached();
-$m->addServer('192.168.39.76', 31211) or die ("Cannot connect to memcached!");
+$m->addServer('192.168.39.45', 31211) or die ("Cannot connect to memcached!");
 $cacheChanged = $m->get('changed');
 if ( $cacheChanged != false OR $cacheChanged == null) {
     require_once "lastfm.php";
